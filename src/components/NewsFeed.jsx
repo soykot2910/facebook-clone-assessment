61fdd144e602adaae59a6b5d9f603dd192b579fe
soykot2910 <md.soykot2910@gmail.com>
2022-08-26 09:10:44 +0600

    create post done

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CreatePost from "./CreatePost";
 import Story from "./Story";
 
 const NewsFeed = () => {
   return (
     <>
       <Story />
+      <CreatePost/>
     </>
   );
 };
